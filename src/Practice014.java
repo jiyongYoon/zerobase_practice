c44778dce33039ef4db2d8ae5292ce99020d91bc
jiyongYoon <jyyoon0615@naver.com>
2022-05-17 23:53:50 +0900

    연습문제 014 수정 //출력을 모아서 하도록 수정함. [학습내용] 1) 사람의 관점으로 움직이는 변수가 있으면 그 변수를 ++, -- 해주면 더 직관적임. 2) 다차원 배열의    행의 길이 = 객체이름.length    열의 길이 = 객체이름[0].length

--- src/Practice014.java
+++ src/Practice014.java
@@ -14,34 +14,36 @@ public class Practice014 {
         int idxn = matrix.length; // 행의 개수
         int currentcnt = 0; // 원소의 개수
         int a = 0; // 카운트
+        ArrayList<Integer> arr = new ArrayList<>(); // 출력을 위해 숫자들을 받아 놓을 리스트
+
         while (true) {
-            // 오른 출력
+            // 오른쪽으로
             for (int i = 0; i < idxm - (2 * a); i++) {
-                System.out.print(matrix[a][i + a] + ",");
+                arr.add(matrix[a][i + a]);
                 currentcnt++;
             }
             if (currentcnt == idxm * idxn) {
                 break;
             }
-            // 아래 출력
+            // 아래로
             for (int i = 0; i < idxn - 1 - (2 * a); i++) {
-                System.out.print(matrix[i + 1 + a][idxm - 1 - a] + ",");
+                arr.add(matrix[i + 1 + a][idxm - 1 - a]);
                 currentcnt++;
             }
             if (currentcnt == idxm * idxn) {
                 break;
             }
-            // 왼 출력
+            // 왼쪽으로
             for (int i = 0; i < idxm - 1 - (2 * a); i++) {
-                System.out.print(matrix[idxn - 1 - a][idxm - 2 - i - a] + ",");
+                arr.add(matrix[idxn - 1 - a][idxm - 2 - i - a]);
                 currentcnt++;
             }
             if (currentcnt == idxm * idxn) {
                 break;
             }
-            // 위 출력
+            // 위쪽으로
             for (int i = 0; i < idxn - 2 - (2 * a); i++) {
-                System.out.print(matrix[idxn - 2 - i - a][a] + ",");
+                arr.add(matrix[idxn - 2 - i - a][a]);
                 currentcnt++;
             }
             if (currentcnt == idxm * idxn) {
@@ -49,6 +51,15 @@ public class Practice014 {
             }
             a++;
         }
+        //출력을 모아서 하도록 수정함.
+        int[] printarr = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            printarr[i] = arr.get(i);
+        }
+        for (int i = 0; i < printarr.length-1; i++) {
+            System.out.print(printarr[i] + ", ");
+        }
+        System.out.println(printarr[printarr.length-1]);
     }
 
     public static void main(String[] args) {
@@ -102,9 +113,9 @@ public class Practice014 {
 }
 
 /*
-        [학습내용]
-        1) 사람의 관점으로 움직이는 변수가 있으면 그 변수를 ++, -- 해주면 더 직관적임.
-        2) 다차원 배열의
-           행의 길이 = 객체이름.length
-           열의 길이 = 객체이름[0].length
+[학습내용]
+1) 사람의 관점으로 움직이는 변수가 있으면 그 변수를 ++, -- 해주면 더 직관적임.
+2) 다차원 배열의
+   행의 길이 = 객체이름.length
+   열의 길이 = 객체이름[0].length
  */
