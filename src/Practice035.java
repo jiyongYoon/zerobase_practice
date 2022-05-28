52c9fc818a93422b331d20a8cce50639e263093a
jiyongYoon <jyyoon0615@naver.com>
2022-05-29 01:19:31 +0900

    연습문제 035 완료. (개선완료) 곱셈을 하는 프로그램(세로로 계산하는 것을 그대로 출력하기)

--- src/Practice035.java
+++ src/Practice035.java
@@ -27,28 +27,37 @@ public class Practice035 {
     public static void times(int a, String b) {
         int[] arr = new int[b.length()];
         int result = a * Integer.parseInt(b);
+        int resultIdx = idx(result);
 
-        space(idx(result)-idx(a));
+        if(idx(result)<=arr.length+1) {
+            resultIdx = arr.length+1;
+        }
+
+        //출력부
+        //수식부분
+        space(resultIdx-idx(a));
         System.out.println(a);
-
         System.out.print("x");
-        space(idx(result)-idx(Integer.parseInt(b))-1);
+        space(resultIdx-idx(Integer.parseInt(b))-1);
         System.out.println(b);
-        line(idx(result));
+        //계산부분
+        line(resultIdx);
         int tmp = 0;
         for (int i = arr.length-1; i >=0; i--) {
             arr[i] = Integer.parseInt(b.substring(i,i+1));
-            space(idx(result)-idx(a*arr[i])-tmp);
+            space(resultIdx-idx(a*arr[i])-tmp);
             System.out.print(a*arr[i]);
             space(tmp);
             System.out.println();
             tmp++;
         }
-        line(idx(result));
-        System.out.println(result);
+        line(resultIdx);
+        //결과부분
+        space((arr.length+1)-idx(result));
+        System.out.print(result);
     }
 
     public static void main(String[] args) {
-        times(123, "123");
+        times(4, "33343");
     }
 }
