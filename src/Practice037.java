496b69b9d372ea5975fe8a5b438294f41c403fc1
jiyongYoon <jyyoon0615@naver.com>
2022-05-31 19:15:43 +0900

    연습문제 037 완료.(정렬종류별로 개선필요) [학습내용] 1. 버블정렬 완료: 맨 마지막 인덱스부터 차례대로 올라오면서 왼쪽에 작은 값이 있으면 바꿈. 이걸 정렬될때까지 반복.

---
+++ src/Practice037.java
@@ -0,0 +1,23 @@
+// 배열을 오름차순으로 정렬하기
+// 버블정렬 사용
+import java.util.Arrays;
+public class Practice037 {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 1, 4, 6, 1};
+        int lidx = 0;
+        for (int i = arr.length-1; i >=0 ; i--) {
+            int idx = arr.length-1;
+            while(idx>lidx) {
+                if (arr[idx] < arr[idx - 1]) {
+                    int tmp = 0;
+                    tmp = arr[idx - 1];
+                    arr[idx - 1] = arr[idx];
+                    arr[idx] = tmp;
+                }
+                idx--;
+            }
+            lidx++;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
