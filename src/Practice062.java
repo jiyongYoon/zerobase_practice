45a93a598ba866aac5f796cb8b4e058dc890b40f
jiyongYoon <jyyoon0615@naver.com>
2022-06-09 16:09:13 +0900

    연습문제 062 완료. //프로그래머스 Lv.1 // 2019 KAKAO BLIND RECRUITMENT // 실패율 [학습내용] TreeMap은 put 할 때 key 값을 오름차순 정렬하여 넣어준다!!

--- src/Practice062.java
+++ src/Practice062.java
@@ -50,7 +50,44 @@ public class Practice062 {
 
         return answer;
     }
+    /* 다른풀이
+    1) 다 쓴 배열은 재활용
+    2) 버블정렬을 통해 정렬할 때 인덱스도 같이 정렬
+    public static int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        double[] tempArr = new double[N];
+        int arrLength = stages.length;
+        int idx = arrLength;
+        double tempD = 0;
+        int tempI = 0;
+        for (int i = 0; i < arrLength; i++) {
+            int stage = stages[i];
+            if (stage != N + 1)
+                answer[stage - 1] += 1;
+        }
+        for (int i = 0; i < N; i++) {
+            int personNum = answer[i];
+            tempArr[i] = (double) personNum / idx;
+            idx -= personNum;
+            answer[i] = i + 1;
+        }
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j < N - i; j++) {
+                if (tempArr[j - 1] < tempArr[j]) {
+                    tempD = tempArr[j - 1];
+                    tempArr[j - 1] = tempArr[j];
+                    tempArr[j] = tempD;
+
+                    tempI = answer[j - 1];
+                    answer[j - 1] = answer[j];
+                    answer[j] = tempI;
+                }
+            }
+        }
+        return answer;
+    }
+     */
     public static void main(String[] args) {
         System.out.println(Arrays.toString(solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3})));
         System.out.println(Arrays.toString(solution(4, new int[]{4,4,4,4,4})));
